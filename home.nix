8edfcd9f8517e453a045e30bf7d25edf56f86803
sumoduduk <sumoduduk13@gmail.com>
2024-06-28 23:24:30 +0700

    feat: install alejandra

--- home.nix
+++ home.nix
@@ -33,6 +33,7 @@ in {
      pkgs.fzf
      pkgs.gnumake
      pkgs.unzip
+     pkgs.alejandra
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
