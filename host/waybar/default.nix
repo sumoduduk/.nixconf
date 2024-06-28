cb4a006e5aae3211e5c9eac8d2171e51fddb7f75
sumoduduk <sumoduduk13@gmail.com>
2024-06-29 02:24:07 +0700

    fix

--- host/waybar/default.nix
+++ host/waybar/default.nix
@@ -3,62 +3,64 @@
     enable = true;
 
     settings = {
-      layer = "top";
-      modules-left = ["customarch" "hyprlandworkspaces"];
-      modules-center = ["clock"];
-      modules-right = ["backlight" "pulseaudio" "bluetooth" "network" "battery"];
-      customarch = {
-        format = "  ";
-        tooltip = false;
-        on-click = "sh $HOME/.config/rofi/bin/powermenu";
-      };
-      hyprlandworkspaces = {
-        format = "{name}";
-        format-icons = {
-          active = "";
-          default = "";
+      mainBar = {
+        layer = "top";
+        modules-left = ["custom/arch" "hyprland/workspaces"];
+        modules-center = ["clock"];
+        modules-right = ["backlight" "pulseaudio" "bluetooth" "network" "battery"];
+        "custom/arch" = {
+          format = "  ";
+          tooltip = false;
+          on-click = "sh $HOME/.config/rofi/bin/powermenu";
         };
-        tooltip = false;
-        all-outputs = true;
-      };
-      clock = {
-        format = "<span color='#b4befe'> </span>{:%H:%M}";
-      };
-      backlight = {
-        device = "intel_backlight";
-        format = "<span color='#b4befe'>{icon}</span> {percent}%";
-        format-icons = ["" "" "" "" "" "" "" "" ""];
-      };
-      pulseaudio = {
-        format = "<span color='#b4befe'>{icon}</span> {volume}%";
-        format-muted = "";
-        tooltip = false;
-        format-icons = {
-          headphone = "";
-          default = ["" "" "󰕾" "󰕾" "󰕾" "" "" ""];
+        "hyprland/workspaces" = {
+          format = "{name}";
+          format-icons = {
+            active = "";
+            default = "";
+          };
+          tooltip = false;
+          all-outputs = true;
         };
-        scroll-step = 1;
-      };
-      bluetooth = {
-        format = "<span color='#b4befe'></span> {status}";
-        format-disabled = "";
-        format-connected = "<span color='#b4befe'></span> {num_connections}";
-        tooltip-format = "{device_enumerate}";
-        tooltip-format-enumerate-connected = "{device_alias}   {device_address}";
-      };
-      network = {
-        interface = "wlo1";
-        format = "{ifname}";
-        format-wifi = "<span color='#b4befe'> </span>{essid}";
-        format-ethernet = "{ipaddr}/{cidr} ";
-        format-disconnected = "<span color='#b4befe'>󰖪 </span>No Network";
-        tooltip = false;
-      };
-      battery = {
-        format = "<span color='#b4befe'>{icon}</span> {capacity}%";
-        format-icons = ["" "" "" "" "" "" "" "" "" ""];
-        format-charging = "<span color='#b4befe'></span> {capacity}%";
-        tooltip = false;
+        clock = {
+          format = "<span color='#b4befe'> </span>{:%H:%M}";
+        };
+        backlight = {
+          device = "intel_backlight";
+          format = "<span color='#b4befe'>{icon}</span> {percent}%";
+          format-icons = ["" "" "" "" "" "" "" "" ""];
+        };
+        pulseaudio = {
+          format = "<span color='#b4befe'>{icon}</span> {volume}%";
+          format-muted = "";
+          tooltip = false;
+          format-icons = {
+            headphone = "";
+            default = ["" "" "󰕾" "󰕾" "󰕾" "" "" ""];
+          };
+          scroll-step = 1;
+        };
+        bluetooth = {
+          format = "<span color='#b4befe'></span> {status}";
+          format-disabled = "";
+          format-connected = "<span color='#b4befe'></span> {num_connections}";
+          tooltip-format = "{device_enumerate}";
+          tooltip-format-enumerate-connected = "{device_alias}   {device_address}";
+        };
+        network = {
+          interface = "wlo1";
+          format = "{ifname}";
+          format-wifi = "<span color='#b4befe'> </span>{essid}";
+          format-ethernet = "{ipaddr}/{cidr} ";
+          format-disconnected = "<span color='#b4befe'>󰖪 </span>No Network";
+          tooltip = false;
+        };
+        battery = {
+          format = "<span color='#b4befe'>{icon}</span> {capacity}%";
+          format-icons = ["" "" "" "" "" "" "" "" "" ""];
+          format-charging = "<span color='#b4befe'></span> {capacity}%";
+          tooltip = false;
+        };
       };
     };
 
