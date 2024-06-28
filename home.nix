3da6a70cb3c5c5e84778864254bd7492a70cfdc4
sumoduduk <sumoduduk13@gmail.com>
2024-06-28 23:37:09 +0700

    fix : alacritty setting

--- home.nix
+++ home.nix
@@ -115,19 +115,30 @@ in {
         };
         selection.save_to_clipboard = true;
         keyboard.bindings = [
-        {key = "v"; mods = "Control"; action = "Paste"}
-        {key = "t"; mods = "Control"; action = "CreateNewWindow"}
+          {
+            key = "v";
+            mods = "Control";
+            action = "Paste";
+          }
+          {
+            key = "t";
+            mods = "Control";
+            action = "CreateNewWindow";
+          }
         ];
         colors = {
-
-selection = {text = "#11111B" ; background = "#F5E0DC"};
-cursor = {text = "#11111B" ; cursor = "#F5E0DC" };
-
-primary = {
-
-foreground ="#CDD6F4";
-background ="#212528";
-};
+          selection = {
+            text = "#11111B";
+            background = "#F5E0DC";
+          };
+          cursor = {
+            text = "#11111B";
+            cursor = "#F5E0DC";
+          };
+          primary = {
+            foreground = "#CDD6F4";
+            background = "#212528";
+          };
         };
       };
     };
