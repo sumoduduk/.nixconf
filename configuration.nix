f5d55484b5e40de8e4f2f7b6159dad09bb20aae8
sumoduduk <sumoduduk13@gmail.com>
2024-06-29 04:49:55 +0700

    feat: add sound

--- configuration.nix
+++ configuration.nix
@@ -69,7 +69,28 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    pavucontrol
+    playerctl
+
+    pulseaudio
   ];
+
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+
+    pulse.enable = true;
+
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+  };
+
+  # Enable sound. Set to `false` if using PipeWire
+  sound.enable = false;
+
+  nixpkgs.config.pulseaudio = true;
 
   # zsh shell
   environment.shells = with pkgs; [zsh];
