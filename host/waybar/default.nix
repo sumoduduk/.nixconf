02c78d7a50328ea529f46b74e63c976f88cb3096
sumoduduk <sumoduduk13@gmail.com>
2024-06-29 02:19:24 +0700

    fix: waybar

--- host/waybar/default.nix
+++ host/waybar/default.nix
@@ -3,127 +3,127 @@
     enable = true;
 
     settings = {
-  layer = "top";
-  modules-left = [ "customarch" "hyprland/workspaces" ];
-  modules-center = [ "clock" ];
-  modules-right = [ "backlight" "pulseaudio" "bluetooth" "network" "battery" ];
-  customarch = {
-    format = "  ";
-    tooltip = false;
-    on-click = "sh $HOME/.config/rofi/bin/powermenu";
-  };
-  hyprland/workspaces = {
-    format = "{name}";
-    format-icons = {
-      active = "";
-      default = "";
+      layer = "top";
+      modules-left = ["customarch" "hyprlandworkspaces"];
+      modules-center = ["clock"];
+      modules-right = ["backlight" "pulseaudio" "bluetooth" "network" "battery"];
+      customarch = {
+        format = "  ";
+        tooltip = false;
+        on-click = "sh $HOME/.config/rofi/bin/powermenu";
+      };
+      hyprlandworkspaces = {
+        format = "{name}";
+        format-icons = {
+          active = "";
+          default = "";
+        };
+        tooltip = false;
+        all-outputs = true;
+      };
+      clock = {
+        format = "<span color='#b4befe'> </span>{:%H:%M}";
+      };
+      backlight = {
+        device = "intel_backlight";
+        format = "<span color='#b4befe'>{icon}</span> {percent}%";
+        format-icons = ["" "" "" "" "" "" "" "" ""];
+      };
+      pulseaudio = {
+        format = "<span color='#b4befe'>{icon}</span> {volume}%";
+        format-muted = "";
+        tooltip = false;
+        format-icons = {
+          headphone = "";
+          default = ["" "" "󰕾" "󰕾" "󰕾" "" "" ""];
+        };
+        scroll-step = 1;
+      };
+      bluetooth = {
+        format = "<span color='#b4befe'></span> {status}";
+        format-disabled = "";
+        format-connected = "<span color='#b4befe'></span> {num_connections}";
+        tooltip-format = "{device_enumerate}";
+        tooltip-format-enumerate-connected = "{device_alias}   {device_address}";
+      };
+      network = {
+        interface = "wlo1";
+        format = "{ifname}";
+        format-wifi = "<span color='#b4befe'> </span>{essid}";
+        format-ethernet = "{ipaddr}/{cidr} ";
+        format-disconnected = "<span color='#b4befe'>󰖪 </span>No Network";
+        tooltip = false;
+      };
+      battery = {
+        format = "<span color='#b4befe'>{icon}</span> {capacity}%";
+        format-icons = ["" "" "" "" "" "" "" "" "" ""];
+        format-charging = "<span color='#b4befe'></span> {capacity}%";
+        tooltip = false;
+      };
     };
-    tooltip = false;
-    all-outputs = true;
-  };
-  clock = {
-    format = "<span color='#b4befe'> </span>{:%H:%M}";
-  };
-  backlight = {
-    device = "intel_backlight";
-    format = "<span color='#b4befe'>{icon}</span> {percent}%";
-    format-icons = ["" "" "" "" "" "" "" "" ""];
-  };
-  pulseaudio = {
-    format = "<span color='#b4befe'>{icon}</span> {volume}%";
-    format-muted = "";
-    tooltip = false;
-    format-icons = {
-      headphone = "";
-      default = ["" "" "󰕾" "󰕾" "󰕾" "" "" ""];
-    };
-    scroll-step = 1;
-  };
-  bluetooth = {
-    format = "<span color='#b4befe'></span> {status}";
-    format-disabled = "";
-    format-connected = "<span color='#b4befe'></span> {num_connections}";
-    tooltip-format = "{device_enumerate}";
-    tooltip-format-enumerate-connected = "{device_alias}   {device_address}";
-  };
-  network = {
-    interface = "wlo1";
-    format = "{ifname}";
-    format-wifi = "<span color='#b4befe'> </span>{essid}";
-    format-ethernet = "{ipaddr}/{cidr} ";
-    format-disconnected = "<span color='#b4befe'>󰖪 </span>No Network";
-    tooltip = false;
-  };
-  battery = {
-    format = "<span color='#b4befe'>{icon}</span> {capacity}%";
-    format-icons = ["" "" "" "" "" "" "" "" "" ""];
-    format-charging = "<span color='#b4befe'></span> {capacity}%";
-    tooltip = false;
-  };
-};     
 
     style = ''
 
-* {
-  border: none;
-  font-family: 'Fira Code', 'Symbols Nerd Font Mono';
-  font-size: 16px;
-  font-feature-settings: '"zero", "ss01", "ss02", "ss03", "ss04", "ss05", "cv31"';
-  min-height: 45px;
-}
+      * {
+        border: none;
+        font-family: 'Fira Code', 'Symbols Nerd Font Mono';
+        font-size: 16px;
+        font-feature-settings: '"zero", "ss01", "ss02", "ss03", "ss04", "ss05", "cv31"';
+        min-height: 45px;
+      }
 
-window#waybar {
-  background: transparent;
-}
+      window#waybar {
+        background: transparent;
+      }
 
-#custom-arch, #workspaces {
-  border-radius: 10px;
-  background-color: #11111b;
-  color: #b4befe;
-  margin-top: 15px;
-  margin-right: 15px;
-  padding-top: 1px;
-  padding-left: 10px;
-  padding-right: 10px;
-}
+      #custom-arch, #workspaces {
+        border-radius: 10px;
+        background-color: #11111b;
+        color: #b4befe;
+        margin-top: 15px;
+        margin-right: 15px;
+        padding-top: 1px;
+        padding-left: 10px;
+        padding-right: 10px;
+      }
 
-#custom-arch {
-  font-size: 20px;
-  margin-left: 15px;
-  color: #b4befe;
-}
+      #custom-arch {
+        font-size: 20px;
+        margin-left: 15px;
+        color: #b4befe;
+      }
 
-#workspaces button {
-  background: #11111b;
-  color: #b4befe;
-}
+      #workspaces button {
+        background: #11111b;
+        color: #b4befe;
+      }
 
-#clock, #backlight, #pulseaudio, #bluetooth, #network, #battery{
-  border-radius: 10px;
-  background-color: #11111b;
-  color: #cdd6f4;
-  margin-top: 15px;
-  padding-left: 10px;
-  padding-right: 10px;
-  margin-right: 15px;
-}
+      #clock, #backlight, #pulseaudio, #bluetooth, #network, #battery{
+        border-radius: 10px;
+        background-color: #11111b;
+        color: #cdd6f4;
+        margin-top: 15px;
+        padding-left: 10px;
+        padding-right: 10px;
+        margin-right: 15px;
+      }
 
-#backlight, #bluetooth {
-  border-top-right-radius: 0;
-  border-bottom-right-radius: 0;
-  padding-right: 5px;
-  margin-right: 0
-}
+      #backlight, #bluetooth {
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+        padding-right: 5px;
+        margin-right: 0
+      }
 
-#pulseaudio, #network {
-  border-top-left-radius: 0;
-  border-bottom-left-radius: 0;
-  padding-left: 5px;
-}
+      #pulseaudio, #network {
+        border-top-left-radius: 0;
+        border-bottom-left-radius: 0;
+        padding-left: 5px;
+      }
 
-#clock {
-  margin-right: 0;
-}
+      #clock {
+        margin-right: 0;
+      }
     '';
   };
 }
