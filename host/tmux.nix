4328b9c6f3b15e41bfd536c26d5674bbccbc64f9
sumoduduk <sumoduduk13@gmail.com>
2024-06-29 21:06:19 +0700

    feat: add tmux

---
+++ host/tmux.nix
@@ -0,0 +1,58 @@
+{pkgs, ...}: {
+  programs.tmux = {
+    enable = true;
+    terminal = "screen-256color";
+
+    # Rather than constraining window size to the maximum size of any client
+    # connected to the *session*, constrain window size to the maximum size of any
+    # client connected to *that window*. Much more reasonable.
+    aggressiveResize = true;
+
+    clock24 = true;
+
+    # Allows for faster key repetition
+    # escapeTime = 50;
+
+    keyMode = "vi";
+    # Overrides the hjkl and HJKL bindings for pane navigation and resizing in VI mode
+    # customPaneNavigationAndResize = true;
+    shortcut = "b";
+    mouse = true;
+
+    plugins = with pkgs.tmuxPlugins; [
+      # gruvbox
+      vim-tmux-navigator
+    ];
+
+    extraConfig = ''
+            unbind %
+            bind | split-window -h
+
+            unbind '"'
+            bind - split-window -v
+
+            unbind r
+            bind r source-file ~/.tmux.conf
+
+            bind -r j resize-pane -D 5
+            bind -r k resize-pane -U 5
+            bind -r l resize-pane -R 5
+            bind -r h resize-pane -L 5
+
+            bind -r m resize-pane -Z
+
+
+      bind-key -T copy-mode-vi 'v' send -X begin-selection # start selecting text with "v"
+      bind-key -T copy-mode-vi 'y' send -X copy-selection # copy text with "y"
+
+      unbind -T copy-mode-vi MouseDragEnd1Pane # don't exit copy mode when dragging with mouse
+
+      # remove delay for exiting insert mode with ESC in Neovim
+      set -sg escape-time 10
+
+                  # auto window rename
+                  set -g automatic-rename
+                  set -g automatic-rename-format '#{pane_current_command}'
+    '';
+  };
+}
