7f3fe59808e10b9575a8447b1f95cb3e948b1d66
sumoduduk <sumoduduk13@gmail.com>
2024-06-30 11:37:53 +0700

    alow unfree in flake

--- flake.nix
+++ flake.nix
@@ -1,31 +1,39 @@
 {
- description = "calista flake config";
+  description = "calista flake config";
 
- inputs = {
-  nixpkgs.url = "nixpkgs/nixos-24.05";
-  home-manager.url = "github:nix-community/home-manager/release-24.05";
-  home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
- };
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+  };
 
- outputs = { self, nixpkgs, home-manager, ... } @inputs:
-  let 
-   lib = nixpkgs.lib;
-   system = "x86_64-linux";
-   pkgs = nixpkgs.legacyPackages.${system};
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
   in {
-  nixosConfigurations = {
-	nixos = lib.nixosSystem {
-	 inherit system;
- 	 specialArgs = { inherit inputs; };
-	 modules = [ ./configuration.nix ];
-	};
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        inherit system;
+        specialArgs = {inherit inputs;};
+        modules = [./configuration.nix];
+      };
+    };
+    homeConfigurations = {
+      calista = home-manager.lib.homeManagerConfiguration {
+        pkgs = import inputs.nixpkgs {
+          system = "x86_64-linux";
+          config = {
+            allowUnfree = true;
+          };
+        };
+        modules = [./home.nix];
+      };
+    };
   };
-  homeConfigurations = {
-	calista = home-manager.lib.homeManagerConfiguration  {
-	 inherit pkgs;
-	 modules = [ ./home.nix ];
-	};
-  };
- };
 }
