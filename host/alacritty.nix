9482e2c6fbb588e9bbe7896579cdc6fe2779b36e
sumoduduk <sumoduduk13@gmail.com>
2024-07-03 01:19:24 +0700

    refactor(home.nix) : make separate file for zsh and terminal

---
+++ host/alacritty.nix
@@ -0,0 +1,48 @@
+{
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font.size = 12;
+      font.normal = {
+        family = "FiraCodeNerdFontPropo";
+        style = "Regular";
+      };
+      window = {
+        opacity = 0.93;
+        dynamic_padding = true;
+        blur = true;
+        padding = {
+          x = 1;
+          y = 2;
+        };
+      };
+      selection.save_to_clipboard = true;
+      keyboard.bindings = [
+        {
+          key = "v";
+          mods = "Control";
+          action = "Paste";
+        }
+        {
+          key = "t";
+          mods = "Control";
+          action = "CreateNewWindow";
+        }
+      ];
+      colors = {
+        selection = {
+          text = "#11111B";
+          background = "#F5E0DC";
+        };
+        cursor = {
+          text = "#11111B";
+          cursor = "#F5E0DC";
+        };
+        primary = {
+          foreground = "#CDD6F4";
+          background = "#212528";
+        };
+      };
+    };
+  };
+}
---
+++ host/zsh.nix
@@ -0,0 +1,25 @@
+{
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      cls = "clear";
+      project = "cd $HOME/File/projects";
+      rustproject = "cd RUST_PROJECT";
+      pythproject = "cd $HOME/File/projects/python_project";
+      webproject = "cd $HOME/File/projects/js_project";
+      vaultobsidian = "cd $VAULT_OBSIDIAN";
+      nixconf = "cd $HOME/.nixconf";
+      getgit = "nvim $HOME/File/projects/game";
+      flakeupdate = "nixconf && sudo nix flake update";
+      flakerebuild = "sudo nixos-rebuild switch --flake .";
+      homerebuild = "home-manager switch --flake .  --show-trace";
+      vimconf = "cd $HOME/File/nvim";
+      syncobsidian = "$RUST_PROJECT/obsidian_github_sync/result/bin/obsidian-github-sync $VAULT_OBSIDIAN";
+    };
+    oh-my-zsh = {
+      enable = true;
+      theme = "dpoggi";
+    };
+  };
+}
